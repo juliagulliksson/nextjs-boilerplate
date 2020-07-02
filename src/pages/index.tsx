7131a2a14cea5e7ca56e08bbe5449178af745c67
julia <julia.gullliksson@gmail.com>
2020-07-02 17:23:25 +0200

    add material ui customization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      color: theme.palette.primary.main,
+    },
+  }),
+);
 
 const Index: React.FC = () => {
-  return <div>Welcome to Next.js!</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Typography variant='h3'>Welcome to Next.js!</Typography>
+    </div>
+  );
 };
 export default Index;
